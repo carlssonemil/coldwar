993b7ad61833aa8f1d76f48af90cc5e86b49a288
CrazyIsEasy <crazyiseasyy@gmail.com>
2021-11-01 15:06:49 -0400

    Added "Hammer and Sickle" Melee

--- src/data/weapons/melee.js
+++ src/data/weapons/melee.js
@@ -1,6 +1,6 @@
 import { ultraProgress, aetherProgress } from '../defaults'
 
-const weapons = ['Knife', 'Sledgehammer', 'Wakizashi', 'E-Tool', 'Machete', 'Baseball Bat', 'Mace', 'Cane', 'Sai', 'Battle Axe', /*'Hammer and Sickle'*/]
+const weapons = ['Knife', 'Sledgehammer', 'Wakizashi', 'E-Tool', 'Machete', 'Baseball Bat', 'Mace', 'Cane', 'Sai', 'Battle Axe', 'Hammer and Sickle']
 const original = ['Knife']
 
 export default weapons.map(weapon => ({
