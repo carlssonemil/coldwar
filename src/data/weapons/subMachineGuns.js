787e85b64748d1d440bd677515017cad70099c6d
CrazyIsEasy <crazyiseasyy@gmail.com>
2022-05-06 16:02:24 -0400

    Added 'UGR' Submachine Gun

--- src/data/weapons/subMachineGuns.js
+++ src/data/weapons/subMachineGuns.js
@@ -1,6 +1,6 @@
 import { ultraProgress, aetherProgress } from '../defaults'
 
-const weapons = ['MP5', 'Milano 821', 'AK-74u', 'KSP 45', 'Bullfrog', 'MAC-10', 'LC10', 'PPSh-41', 'OTs 9', 'TEC-9', 'LAPA']
+const weapons = ['MP5', 'Milano 821', 'AK-74u', 'KSP 45', 'Bullfrog', 'MAC-10', 'LC10', 'PPSh-41', 'OTs 9', 'TEC-9', 'LAPA', 'UGR']
 const original = ['MP5', 'Milano 821', 'AK-74u', 'KSP 45', 'Bullfrog']
 
 export default weapons.map(weapon => ({
